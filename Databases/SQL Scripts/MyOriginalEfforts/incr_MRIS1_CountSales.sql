2b20a7a1187f84d8d44ea7d160b811296e8a4e4b
Dan Sekelik <dsekelik@gmail.com>
2017-01-17 20:24:20 -0500

    Remove unused files from SQL Scripts folder

--- Databases/SQL Scripts/MyOriginalEfforts/incr_MRIS1_CountSales.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE incr_MRIS1_CountSales
-AS
-SELECT MRIS.Address As Address, count(MRIS.Address) As SalesHistCount, max(MRIS.CloseDate) As MaxCloseDt
-FROM Source_MRIS MRIS
-GROUP BY MRIS.Address;
